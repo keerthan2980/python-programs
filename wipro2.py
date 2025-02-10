1aa458c345a6f09d81057a3c90f29fb7cce7e1f2
BONDUGULA KEERTHAN KUMAR <144529963+keerthan2980@users.noreply.github.com>
2025-02-10 13:37:49 +0530

    Add files via upload

---
+++ wipro2.py
@@ -0,0 +1,12 @@
+string=input("enter the string:")
+target=input("enter the sting to find the count")
+count_target=string.strip().split().count(target)
+print(count_target)
+
+"""
+
+output:
+enter the string:welcome to my to world my hello my
+enter the sting to find the countmy
+3
+"""
